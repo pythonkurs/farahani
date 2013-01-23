bc3e1078550999019a85b6cfc3496848811b2502
Hossein <me@hfarahani.com>
2013-01-23 12:45:03 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,16 +3,16 @@ import sys, os
 
 version = '0.1dev'
 
-setup(name='schmitt',
+setup(name='farahani',
       version=version,
       description="SciLifeLab python course 2013",
       long_description="""\
 """,
       classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
       keywords='scilifelab bioinformatics',
-      author='Thomas Schmitt',
-      author_email='Thomas.Schmitt@scilifelab.se',
-      url='http://github.com/pythonkurs/schmitt',
+      author='Hossein Farahani',
+      author_email='farahani@scilifelab.se',
+      url='http://github.com/pythonkurs/farahani',
       license='',
       packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
       include_package_data=True,
